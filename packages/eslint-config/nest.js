38b37c361b24d8b9d1f690ce78414fce32ce19d2
Jim Cramer <remarcmij@gmail.com>
2025-11-22 22:09:56 +0100

    Fix deprecation warning

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,18 +1,21 @@
-import { config as baseConfig } from './base.js';
-import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import { config as baseConfig } from './base.js';
 
-export const config = tseslint.config(...baseConfig, {
-  languageOptions: {
-    globals: {
-      ...globals.node,
-      ...globals.jest,
+// ...existing code...
+export const config = [
+  ...baseConfig,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-});
+];
