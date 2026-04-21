234f3ebc7f0d4972834c9a75a9b28e33ab334090
Jim Cramer <remarcmij@gmail.com>
2026-04-21 21:36:27 +0200

    Upgrade typescript to v6

--- apps/dict-compiler/src/index.ts
+++ apps/dict-compiler/src/index.ts
@@ -3,15 +3,12 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { Compiler } from './compiler/Compiler.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const DICT_PATH = '../dict';
-const DEST_PATH = '../json';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dictPath = path.join(__dirname, '../dict');
+const destPath = path.join(__dirname, '../json');
 
 async function main() {
-  const globPattern = path
-    .join(__dirname, DICT_PATH, '**/*.md')
-    .replace(/\\/g, '/');
+  const globPattern = path.join(dictPath, '**/*.md').replace(/\\/g, '/');
 
   const startTime = process.hrtime();
 
@@ -19,7 +16,7 @@ async function main() {
 
   const promises = filenames.map((inFile) => {
     const stem = path.basename(inFile, '.md').toLowerCase();
-    const outFile = path.join(__dirname, DEST_PATH, `${stem}.json`);
+    const outFile = path.join(destPath, `${stem}.json`);
     const compiler = new Compiler(inFile, outFile);
     return compiler.run();
   });
